b2838e7cc725877e66fff64f372f478c57df1193
yourui123 <2284182274@qq.com>
2017-11-10 09:58:06 +0800

    工具更新

---
+++ src/main/java/cn/ces/tool/AttributesBean.java
@@ -0,0 +1,26 @@
+package cn.ces.tool;
+
+
+public class AttributesBean {
+	//�˵���ַ
+	private String url;
+	private int sortCode;
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public int getSortCode() {
+		return sortCode;
+	}
+
+	public void setSortCode(int sortCode) {
+		this.sortCode = sortCode;
+	}
+
+	
+}
