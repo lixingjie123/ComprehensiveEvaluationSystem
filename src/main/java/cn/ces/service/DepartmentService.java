6d06f981dccef5e77f04e1d7a6136e1d9845e6eb
lixingjie123 <lixingjielover@qq.com>
2017-11-14 16:58:26 +0800

    修复DEPT的bug

--- src/main/java/cn/ces/service/DepartmentService.java
+++ src/main/java/cn/ces/service/DepartmentService.java
@@ -9,8 +9,6 @@ import org.springframework.stereotype.Service;
 
 import cn.ces.dao.DepartmentDao;
 import cn.ces.entity.Department;
-import cn.ces.entity.Power;
-import cn.ces.entity.Users;
 
 @Service
 public class DepartmentService {
@@ -21,13 +19,7 @@ public class DepartmentService {
 	public Map<String,Object> selectDepartmentall(int pageIndex, int pageSize,String dept_name){
         Map<String,Object> result = new HashMap<String,Object>();
         int total = departmentDao.selectDepartmentcount(dept_name);
-        List<Department> rows = departmentDao.selectDepartment(pageSize,pageIndex,dept_name);
-        for(int i=0;i<rows.size();i++){
-        	Department dept = departmentDao.selectDepartmentByid(rows.get(i).getDept_id());
-        	if(dept!=null){
-        	rows.get(i).setDept_name(dept.getDept_name());
-        	}
-        }
+        List<Department> rows = departmentDao.selectDepartment(pageIndex,pageSize,dept_name);
         result.put("total",total);
         result.put("rows",rows);
         return result;
